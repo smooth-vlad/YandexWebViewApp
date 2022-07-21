b0cf2165e1732f4a485b230b4f8e4d0ef853a80b
Vladislav Gladkiy <gladkyvladis@yandex.com>
2022-07-21 15:54:07 +0300

    Add translations

--- app/src/main/java/com/android/webviewapp/ClosingAppDialogFragment.kt
+++ app/src/main/java/com/android/webviewapp/ClosingAppDialogFragment.kt
@@ -8,11 +8,11 @@ import androidx.fragment.app.DialogFragment
 class ClosingAppDialogFragment(private val onConfirmAction: () -> Unit) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
-            .setMessage("Are you sure want to exit?")
-            .setPositiveButton("Yes") { dialog, which ->
+            .setMessage(getString(R.string.exit_confirmation_text))
+            .setPositiveButton(getString(R.string.exit_confirmation_confirm)) { dialog, which ->
                 onConfirmAction()
             }
-            .setNegativeButton("Cancel") { dialog, which ->
+            .setNegativeButton(getString(R.string.exit_confirmation_cancel)) { dialog, which ->
                 // nothing
             }
         return builder.create()
